3dfcd60488c575583bbe71689db7226a0b4cdae9
seoseuo <seoseuo@naver.com>
2024-09-06 15:45:08 +0900

    DAO - 조회 기능 DAO - 추가 기능

--- WEB_SERVER/PROJECTS/BoardWeb/src/main/java/com/ssamz/biz/user/InsertUserTest.java
+++ WEB_SERVER/PROJECTS/BoardWeb/src/main/java/com/ssamz/biz/user/InsertUserTest.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+import com.ssamz.biz.common.JDBCUtil;
+
 public class InsertUserTest {
 
 	public static void main(String[] args) {
@@ -15,46 +17,31 @@ public class InsertUserTest {
 		PreparedStatement stmt = null;
 
 		try {
-			// JDBC 1단계: 드라이버 객체 로딩
-			DriverManager.registerDriver(new org.h2.Driver());
 
-			// JDBC 2단계 : 커넥션 연결
-			String jdbcUrl = "jdbc:h2:tcp://localhost/~/test";
-			conn = DriverManager.getConnection(jdbcUrl, "sa", "");
-
-			if (conn != null) {
-				System.out.println("H2 연결 성공 : " + conn.toString());
-			}
-
+			conn = JDBCUtil.getConnection();
 			// JDBC 3단계 : Statement 생성
 			String sql = "INSERT INTO USERS VALUES(?,?,?,?)";
 			stmt = conn.prepareStatement(sql);
 
 			// JDBC 4단계 : SQL 전송
 			// ? 값 설정
-			stmt.setString(1, "ssamz");
+			stmt.setString(1, "ssamz3");
 			stmt.setString(2, "ssamz123");
 			stmt.setString(3, "쌤즈");
 			stmt.setString(4, "ADMIN");
 
 			// SQL 전송
 			int count = stmt.executeUpdate();
+			
 			System.out.println(count + "건 데이터 처리 성공!");
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			JDBCUtil.close(stmt, conn);
 		}
 
-		finally {
-			// JDBC 5단계 : 연결 해제
-			try {
-				stmt.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 	}
 
 }
---
+++ WEB_SERVER/PROJECTS/BoardWeb/src/main/java/com/ssamz/biz/user/SelectUserTest3.java
@@ -0,0 +1,12 @@
+package com.ssamz.biz.user;
+
+public class SelectUserTest3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		UserDAO dao = new UserDAO();
+		dao.getUserList();
+	}
+
+}
