3b987c9642c883717a20db9c7d7741250743ee66
seoseuo <seoseuo@naver.com>
2024-09-10 11:09:20 +0900

    LoginServlet : 로그인 기능 구현

--- WEB_SERVER/PROJECTS/BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
+++ WEB_SERVER/PROJECTS/BoardWeb/src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -37,16 +37,39 @@ public class LoginServlet extends HttpServlet {
 //	}
 
 	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	// 로그인 서블
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// 1.사용자 입력 정보 추출
 		String id = request.getParameter("id");
-		
+		String password = request.getParameter("password");
+
 		// 2. DB 연동 처리
 		UserVO vo = new UserVO();
 		vo.setId(id);
 
 		UserDAO dao = new UserDAO();
-		UserVO user = dao.getUser(vo);
+		UserVO user = dao.getUser(vo); // ID를 조회하여 일치하는 객체
+
+		// 3. 응답화면 구성
+
+		if (user != null) {
+			// 로그인 성공 경우
+			if (user.getPassword().equals(password)) {
+
+				System.out.println(user.getName() + "님 로그인 환영! <br>");
+				System.out.println("<a href='/getBoardList.do'>글 목록 이동</a>");
+			}
+			// 비밀번호가 틀린 경우
+			else {
+				System.out.println("비밀번호 오류입니다.<br>");
+				System.out.println("<a href='/'></a>");
+			}
+		} else { // 아이디가 틀린 경우
+			System.out.println("아이디 오류입니다.<br>");
+			System.out.println("<a href='/'>다시 로그인</a>");
+		}
+
 	}
 
 	/**
